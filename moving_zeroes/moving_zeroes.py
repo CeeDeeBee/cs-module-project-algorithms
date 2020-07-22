8e9b6fd7f1d0157e26eaceaa43cf082074dda5fc
CeeDeeBee <cdbarnes98@gmail.com>
2020-07-22 15:56:48 -0400

    Add moving zeros first pass

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,19 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    new_arr = [0] * len(arr)
 
-    pass
+    loc = 0
+    for i in arr:
+        if i is not 0:
+            new_arr[loc] = i
+            loc += 1
+
+    return new_arr
 
 
 if __name__ == '__main__':
