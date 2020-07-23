106f541d879b99db4db5d1418d557571879d8ef4
CeeDeeBee <cdbarnes98@gmail.com>
2020-07-23 17:19:15 -0400

    Add moving zeros optimized implementation

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -6,15 +6,32 @@ Returns: a List of integers
 
 def moving_zeroes(arr):
     # Your code here
-    new_arr = [0] * len(arr)
+    # new_arr = [0] * len(arr)
 
-    loc = 0
-    for i in arr:
-        if i is not 0:
-            new_arr[loc] = i
-            loc += 1
+    # loc = 0
+    # for i in arr:
+    #     if i is not 0:
+    #         new_arr[loc] = i
+    #         loc += 1
 
-    return new_arr
+    # return new_arr
+
+    # optimized
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        if not arr[left] and arr[right]:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        else:
+            if arr[left]:
+                left += 1
+            if not arr[right]:
+                right -= 1
+
+    return arr
 
 
 if __name__ == '__main__':
