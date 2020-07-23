09cb320c8115d359e02b6f4f3d249ee9c3e6155a
CeeDeeBee <cdbarnes98@gmail.com>
2020-07-22 21:20:17 -0400

    Add eating cookies first pass

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,27 @@
 Input: an integer
 Returns: an integer
 '''
+
+
 def eating_cookies(n):
     # Your code here
+    if n <= 0:
+        return 1
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
 
-    pass
+    m3 = eating_cookies(n - 3)
+    m2 = eating_cookies(n - 2)
+    m1 = eating_cookies(n - 1)
+
+    return m1 + m2 + m3
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
